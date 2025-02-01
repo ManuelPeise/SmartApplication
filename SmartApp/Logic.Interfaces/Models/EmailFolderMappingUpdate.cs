45716b47883d18b0b064bce06cae3f1edaac1bd1
manuel peise <manuel.p80@gmx.de>
2025-02-01 13:54:20 +0100

    cleanup and remove currently unused stuff

--- SmartApp/Logic.Interfaces/Models/EmailFolderMappingUpdate.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Logic.Interfaces.Models
-{
-    public class EmailFolderMappingUpdate
-    {
-        public string SettingsGuid { get; set; } = string.Empty;
-        public List<FolderMapping> Mappings { get; set; } = new List<FolderMapping>();
-    }
-}
